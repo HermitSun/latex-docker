158130b8b65e5e4992aee67682ca227cdab9d555
鱼无盐 <syl1887415157@126.com>
2021-09-22 23:01:56 +0800

    refactor: reduce image size

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 # fix version
-FROM ghcr.io/xu-cheng/texlive-full:20210901
+FROM alpine:3.13.6
+
+# only support full now
+ARG scheme=full
+ENV PATH="/opt/texlive/texdir/bin/x86_64-linuxmusl:${PATH}"
 
 LABEL org.opencontainers.image.title="Docker Image of TeXLive with zh_CN Support" \
   org.opencontainers.image.authors="Cheng Xu <copyright@xuc.me>, HermitSun <syl1887415157@126.com>" \
@@ -10,11 +14,15 @@ LABEL org.opencontainers.image.title="Docker Image of TeXLive with zh_CN Support
 # ignore files in .dockerignore
 COPY . /
 
-# prepare fonts
-RUN cp /opt/texlive/texdir/texmf-var/fonts/conf/texlive-fontconfig.conf /etc/fonts/conf.d/09-texlive.conf \
+# install texlive first
+# and then prepare fonts
+RUN /setup.sh ${scheme} \
+    && cp /opt/texlive/texdir/texmf-var/fonts/conf/texlive-fontconfig.conf /etc/fonts/conf.d/09-texlive.conf \
     && mv /adobe/ /usr/share/fonts/ \
     && mv /linux/ /usr/share/fonts/ \
     && mv /mac/ /usr/share/fonts/ \
     && mv /win/ /usr/share/fonts/ \
     && mv /common/ /usr/share/fonts/ \
     && fc-cache -fsv
+
+CMD ["/bin/sh"]
---
+++ setup.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+set -e
+set -o pipefail
+
+scheme="$1"
+
+retry() {
+  retries=$1
+  shift
+
+  count=0
+  until "$@"; do
+    exit=$?
+    wait="$(echo "2^$count" | bc)"
+    count="$(echo "$count + 1" | bc)"
+    if [ "$count" -lt "$retries" ]; then
+      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
+      sleep "$wait"
+    else
+      echo "Retry $count/$retries exited $exit, no more retries left."
+      return "$exit"
+    fi
+  done
+}
+
+echo "==> Install system packages"
+apk --no-cache add \
+  bash \
+  ghostscript \
+  gnupg \
+  graphviz \
+  openjdk11-jre-headless \
+  perl \
+  py-pygments \
+  python2 \
+  python3 \
+  tar \
+  ttf-freefont \
+  wget \
+  xz
+
+# Dependencies needed by latexindent
+apk --no-cache add \
+  perl-log-dispatch \
+  perl-log-log4perl \
+  perl-namespace-autoclean \
+  perl-params-validationcompiler \
+  perl-specio \
+  perl-unicode-linebreak \
+  perl-yaml-tiny
+apk --no-cache \
+  --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing \
+  add \
+  perl-file-homedir
+
+echo "==> Install TeXLive"
+mkdir -p /tmp/install-tl
+cd /tmp/install-tl
+MIRROR_URL="$(wget -q -S -O /dev/null http://mirror.ctan.org/ 2>&1 | sed -ne 's/.*Location: \(\w*\)/\1/p' | head -n 1)"
+wget -nv "${MIRROR_URL}systems/texlive/tlnet/install-tl-unx.tar.gz"
+wget -nv "${MIRROR_URL}systems/texlive/tlnet/install-tl-unx.tar.gz.sha512"
+wget -nv "${MIRROR_URL}systems/texlive/tlnet/install-tl-unx.tar.gz.sha512.asc"
+gpg --no-default-keyring --keyring trustedkeys.kbx --import /texlive_pgp_keys.asc
+gpgv ./install-tl-unx.tar.gz.sha512.asc ./install-tl-unx.tar.gz.sha512
+sha512sum -c ./install-tl-unx.tar.gz.sha512
+mkdir -p /tmp/install-tl/installer
+tar --strip-components 1 -zxf /tmp/install-tl/install-tl-unx.tar.gz -C /tmp/install-tl/installer
+retry 3 /tmp/install-tl/installer/install-tl -scheme "$scheme" -profile=/texlive.profile
+
+# Install additional packages for non full scheme
+if [ "$scheme" != "full" ]; then
+  tlmgr install \
+    collection-fontsrecommended \
+    collection-fontutils \
+    biber \
+    biblatex \
+    latexmk \
+    texliveonfly
+fi
+
+# System font configuration for XeTeX and LuaTeX
+# Ref: https://www.tug.org/texlive/doc/texlive-en/texlive-en.html#x1-330003.4.4
+ln -s /opt/texlive/texdir/texmf-var/fonts/conf/texlive-fontconfig.conf /etc/fonts/conf.d/09-texlive.conf
+fc-cache -fv
+
+echo "==> Clean up"
+rm -rf \
+  /opt/texlive/texdir/install-tl \
+  /opt/texlive/texdir/install-tl.log \
+  /opt/texlive/texdir/texmf-dist/doc \
+  /opt/texlive/texdir/texmf-dist/source \
+  /opt/texlive/texdir/texmf-var/web2c/tlmgr.log \
+  /root/.gnupg \
+  /setup.sh \
+  /texlive.profile \
+  /texlive_pgp_keys.asc \
+  /tmp/install-tl
